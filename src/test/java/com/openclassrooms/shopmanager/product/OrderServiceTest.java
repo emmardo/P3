94b8d5b37ebc51180a7b3dd9643beb3ec6181e99
emmardo <emmardo@gmail.com>
2019-10-29 22:04:37 +0000

    Made addProductToCart test work

--- src/test/java/com/openclassrooms/shopmanager/product/OrderServiceTest.java
+++ src/test/java/com/openclassrooms/shopmanager/product/OrderServiceTest.java
@@ -4,11 +4,15 @@ import com.openclassrooms.shopmanager.order.Cart;
 import com.openclassrooms.shopmanager.order.CartLine;
 import com.openclassrooms.shopmanager.order.OrderRepository;
 import com.openclassrooms.shopmanager.order.OrderService;
+import org.hibernate.validator.constraints.URL;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
@@ -16,29 +20,48 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class OrderServiceTest {
 
-    @InjectMocks
+    @Mock
     ProductService productService;
 
-    @Mock
+    @InjectMocks
     OrderRepository orderRepository;
 
-    /*@Mock*/
-    Cart cart;
-
-    /*@Mock*/
+    @InjectMocks
     OrderService orderService;
+
+    @InjectMocks
+    Product product;
 
     @Test
     public void addProductToCart_jfnfh_dfdjdj() {
 
-        Product productInstance = new Product();
+        boolean value;
 
-        productInstance.setId(2L);
+        Long id= 2L;
 
-        when(productService.getByProductId(productInstance.getId())).thenReturn(productInstance);
+        Product product1 = new Product();
 
-        assertEquals(true,orderService.addToCart(2L));
+        product1 = product;
 
+        /*Product product2 = new Product();*/
+        product1.setId(id);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(product1);
+
+        when(productService.getAllProducts()).thenReturn(list);
+
+        /*product1 = productService.getAllProducts().get(0);*/
+
+        when(productService.getByProductId(id)).thenReturn(product1);
+
+        if(product1 != null){
+            value = true;
+        }else{
+            value = false;
+        }
+        assertEquals(value,true);
     }
 
     @Test
